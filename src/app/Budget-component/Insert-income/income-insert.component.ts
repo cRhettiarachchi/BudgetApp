c15c5454b57f7af8a0a07ed9971e4d0007fe3821
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-11 20:40:32 +0530

    some minor updates

---
+++ src/app/Budget-component/Insert-income/income-insert.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-insert-income',
+  templateUrl: './income-insert.component.html',
+  styleUrls: ['./income-insert.component.css']
+})
+export class IncomeInsertComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BudgetTotalComponent } from './Budget-component/Total/budget-total.component';
+import {IncomeInsertComponent} from './Budget-component/Insert-income/income-insert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BudgetTotalComponent
+    BudgetTotalComponent,
+    IncomeInsertComponent
   ],
   imports: [
     BrowserModule,
