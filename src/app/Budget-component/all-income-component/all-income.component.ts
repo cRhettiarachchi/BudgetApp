3b289396b2c119b2e63fb1d0f32e2498fecef406
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-12 06:31:56 +0530

    some updates

---
+++ src/app/Budget-component/all-income-component/all-income.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-all-income',
+    templateUrl: './all-income.component.html',
+    styleUrls: ['./all-income.component.css']
+})
+export class AllIncomeComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AllIncomeComponent } from './Budget-component/all-income-component/all-income.component';
+import { DateComponent } from './Budget-component/date-component/date.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BudgetTotalComponent } from './Budget-component/Total/budget-total.component';
 import { IncomeInsertComponent } from './Budget-component/Insert-income/income-insert.component';
+import { AllExpensesComponentComponent } from './Budget-component/all-expenses-component/all-expenses-component.component';
+import { AllExpensesComponent } from './Budget-component/all-expenses/all-expenses.component';
 
 
 
@@ -12,7 +16,11 @@ import { IncomeInsertComponent } from './Budget-component/Insert-income/income-i
   declarations: [
     AppComponent,
     BudgetTotalComponent,
-    IncomeInsertComponent
+    IncomeInsertComponent,
+    DateComponent,
+    AllIncomeComponent,
+    AllExpensesComponentComponent,
+    AllExpensesComponent
   ],
   imports: [
     BrowserModule,
