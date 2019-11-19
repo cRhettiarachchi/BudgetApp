41d93538b6d64da230203124d82e695826129222
Charith Randika Hettiarachchi <charith.rhettiarachchi@gmail.com>
2019-11-19 15:57:25 +0530

    cleaning up the code

--- src/app/services/values-service.service.ts
+++ src/app/services/values-service.service.ts
@@ -17,15 +17,17 @@ export class ValuesServiceService {
   private subjectTotal = new Subject<TotalModel>();
   private totId = '5dd2b6c0050a10465cc8ef52';
 
-
-  getUrl = 'http://localhost:8080/get/values'; // The url of the get Request
+ /* All the URL variables  */
+  getUrl = 'http://localhost:8080/get/values';
   postUrl = 'http://localhost:8080/put/values';
   deleteUrl = 'http://localhost:8080/delete/value/';
   getTotalUrl = 'http://localhost:8080/total/get/';
-  constructor(private http: HttpClient) { } // Http client dependency injection
+  putTotalUrl = 'http://localhost:8080/total/put/';
+  /* ----------------------------------------------- */
+
+  constructor(private http: HttpClient) { }
 
   getAllvalues() { // method to get all the values
-    // calling the get from the url
     this.http.get<any[]>(this.getUrl)
       .pipe(map((values) => {
         return values.map(value => {
@@ -41,51 +43,46 @@ export class ValuesServiceService {
       this.moneyValues = value;
       this.subjectValue.next([...this.moneyValues]); // call the observable
     });
-  }
-  getTotal() {
+  } // end of get all budget values
+  getTotal() { // Method to retrieve the total
     this.http.get<any>(this.getTotalUrl + this.totId)
       .subscribe(value => {
         this.total = value;
-        console.log('this is values ' + this.total);
         this.subjectTotal.next({...this.total});
       });
-    // this.http.get<{ total: number }>(this.getTotalUrl).subscribe(response => {
-    //   total = response.total;
-    // });
-  }
+  } // End of retrieve total
 
-  updateValues(): Observable<BudgetModel[]> {
-    return this.subjectValue.asObservable();
+  updateValues(): Observable<BudgetModel[]> { // method for the observable return
+    return this.subjectValue.asObservable(); // this will pass an oberservale
   }
 
   retrieveTotal(): Observable<TotalModel> {
     return this.subjectTotal.asObservable();
   }
 
-  addValue(amount: number, description: string, type: string) {
-    const value: BudgetModel = new BudgetModel(amount, description, type);
-    console.log(value);
-    this.http.post<{message: string, id: string}>(this.postUrl, value).subscribe(response => {
-      value.id = response.id;
-      this.moneyValues.push(value);
-      this.subjectValue.next([...this.moneyValues]);
+  addValue(amount: number, description: string, type: string) { // Add a budget value to the server
+    const value: BudgetModel = new BudgetModel(amount, description, type); // Read values and create new object
+    this.http.post<{message: string, id: string}>(this.postUrl, value).subscribe(response => { // post call
+      value.id = response.id; // update the value id with the returning id
+      this.moneyValues.push(value); // push the budget object to the array
+      this.subjectValue.next([...this.moneyValues]); // pass it as an observable
     });
-  }
+  } // end of the add value method
   updateTotal(tot: number, type: string) {
-    if (type === 'income') {
+    if (type === 'income') { // to update the total value of the valueServices
       this.total.total = (+this.total.total) + (+tot);
-      console.log('after the if condition ' + typeof(this.total));
     } else {
       this.total.total -= tot;
     }
-    this.http.patch<{ message: string }>('http://localhost:8080/total/put/' + this.totId, this.total).subscribe(msg => {
-      console.log(msg.message);
+    this.updateTot(); // call the updateTot to update the total
+  }
+  updateTot() { // created this method because the patch call was redundant
+    this.http.patch<{ message: string }>( this.putTotalUrl + this.totId, this.total).subscribe(msg => {
       this.subjectTotal.next(this.total);
     });
-    // this.total = type === 'income' ? this.total + total : this.total - total;
   }
 
-  deleteValue(id: string) {
+  deleteValue(id: string) { // method to delete a value
     const message = this.http.delete<{message: string}>(this.deleteUrl + id).subscribe(msg => {
       const updatedValues = this.moneyValues.filter(value => value.id !== id);
       this.moneyValues = updatedValues;
@@ -94,15 +91,12 @@ export class ValuesServiceService {
     this.deleteTotal(id);
   }
 
-  deleteTotal(id: string){
+  deleteTotal(id: string) { // change update value on deleting a component
     if(this.moneyValues.find(p => p.id === id).type === 'income') {
       this.total.total -= this.moneyValues.find(p => p.id === id).amount;
     } else {
       this.total.total = (+this.total.total) + (+this.moneyValues.find(p => p.id === id).amount);
     }
-    this.http.patch<{ message: string }>('http://localhost:8080/total/put/' + this.totId, this.total).subscribe(msg => {
-      console.log(msg.message);
-      this.subjectTotal.next(this.total);
-    });
+    this.updateTot();
   }
 }
